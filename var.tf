b4f956b56852d14d307ec74c7df28d55a1cf33c1
mtlmook <126923583+mtlmook@users.noreply.github.com>
2023-03-27 18:42:29 -0400

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,20 +1,18 @@
 variable "project_id" {
   description = "The ID of the GCP project."
-  default     = "avian-amulet-378416"
+  default     = "learning-project-381703"
 }
 
 variable "bucket_name" {
   description = "A list of unique names for the GCP storage buckets."
   type        = list(string)
   default     = [
-    "my-test-bucket-latest8999",
-    "mytestbucket8888",
-    "montrealcollege-927218-for-demo",
-    "bucket-full-of-happiness"
+    "mcitbucket",
+    
 
   ]
 }
 variable "bucket_location" {
   description = "The location of the GCP storage buckets."
-  default     = "US"
+  default     = "northamerica-northeast1"
 }
